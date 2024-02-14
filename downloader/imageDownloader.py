267f7f9d804baba1d26c4dec8190b935c66b3cad
faklein <fabian.klein@1und1.de>
2024-02-14 14:26:10 +0100

    first commit

---
+++ downloader/imageDownloader.py
@@ -0,0 +1,65 @@
+import requests
+import logging
+import os
+from typing import List
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_path(path: str | os.PathLike):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
+class ImageDownloader:
+
+    @staticmethod
+    def download_image_from_url(
+        img_url: str, save_path: str | os.PathLike, override: bool = True
+    ):
+        img = ImageDownloader._download_image(img_url)
+        if img:
+            img_name = img_url.split("/")[-1]
+            save_path = str(save_path) + "/" + img_name
+            if os.path.isfile(save_path) and not override:
+                LOGGER.warning(
+                    "Image not saved! File " + save_path + " already exists!"
+                )
+            else:
+                with open(save_path, mode="wb") as img_f:
+                    img_f.write(img)
+
+    @staticmethod
+    def download_image_from_file(
+        filename: str | os.PathLike, save_path: str | os.PathLike, override: bool = True
+    ):
+        if not os.path.isfile(get_path(filename)):
+            LOGGER.error("File does not exist")
+            raise FileNotFoundError
+
+        if not os.path.isdir(save_path):
+            LOGGER.error("Path does not exist")
+            raise FileNotFoundError
+
+        save_path = get_path(save_path)
+        img_urls = ImageDownloader._get_urls_from_file(get_path(filename))
+        for img_url in img_urls:
+            if img_url:
+                ImageDownloader.download_image_from_url(img_url, save_path, override)
+
+    @staticmethod
+    def _download_image(img_url: str) -> bytes | None:
+        try:
+            response = requests.get(img_url, timeout=5)
+            response.raise_for_status()
+            return response.content
+        except requests.exceptions.RequestException as e:
+            LOGGER.error(
+                f"The following Exception happened: {type(e).__name__} for Url {img_url}\n"
+                f"Please check URLs"
+            )
+
+    @staticmethod
+    def _get_urls_from_file(filename: str | os.PathLike) -> List[str]:
+        with open(get_path(filename), mode="r") as f:
+            urls = [img_url.strip("\n") for img_url in f if img_url]
+        return urls
---
+++ tests/test_imageDownloader.py
@@ -0,0 +1,124 @@
+import pytest
+from pathlib import Path
+from downloader.imageDownloader import ImageDownloader
+from unittest.mock import patch
+
+
+def get_path(filename: str) -> Path:
+    return Path(__file__).parent / filename
+
+
+@pytest.fixture
+def fake_dir(tmp_path):
+    temp_dir = tmp_path / "images"
+    temp_dir.mkdir()
+    return temp_dir
+
+
+@pytest.fixture
+def get_not_existing_dir():
+    return "not_existing_dir"
+
+
+@pytest.fixture(name="existing_file")
+def get_existing_url_file():
+    return get_path("files/url_file.txt")
+
+
+@pytest.fixture(name="not_existing_file")
+def get_not_existing_url_file():
+    return get_path("no_existing_file.txt")
+
+
+@pytest.fixture(name="mixed_urls")
+def get_mixed_url_file():
+    return get_path("files/mixed_url_file.txt")
+
+
+@pytest.fixture(name="working_url")
+def get_valid_url():
+    return "https://interactives.natgeofe.com/high-touch/ngm-23-YIP/builds/main/img/photos/MM9747_221026_13114.jpg"
+
+
+def test_download_from_url_successful(fake_dir, working_url):
+    with patch("logging.Logger.error") as mock_logger:
+        ImageDownloader.download_image_from_url(working_url, fake_dir)
+        mock_logger.assert_not_called()
+        assert fake_dir.exists()
+        assert len([*fake_dir.iterdir()]) == 1
+
+
+def test_download_from_url_missing_schema(fake_dir):
+    with patch("logging.Logger.error") as mock_logger:
+        ImageDownloader.download_image_from_url(
+            "www.interactives.natgeofe.com/high-touch/ngm-23-YIP/builds/main/img/photos/MM9747_221026_13114.jpg",
+            fake_dir,
+        )
+        mock_logger.assert_called_once()
+        mock_logger.assert_called_with(
+            "The following Exception happened: MissingSchema for Url www.interactives.natgeofe.com/high-touch/ngm-23-YIP/builds/main/img/photos/MM9747_221026_13114.jpg\n"
+            "Please check URLs"
+        )
+        assert fake_dir.exists()
+        assert len([*fake_dir.iterdir()]) == 0
+
+
+def test_download_from_url_not_successful(fake_dir):
+    with patch("logging.Logger.error") as mock_logger:
+        ImageDownloader.download_image_from_url(
+            "https://interactives.natgeofe.com/thisdoesnotexist.jpg", fake_dir
+        )
+
+        mock_logger.assert_called_once()
+        mock_logger.assert_called_with(
+            "The following Exception happened: HTTPError for Url https://interactives.natgeofe.com/thisdoesnotexist.jpg\n"
+            "Please check URLs"
+        )
+        assert fake_dir.exists()
+        assert len([*fake_dir.iterdir()]) == 0
+
+
+def test_download_from_file_successful(fake_dir, existing_file):
+    ImageDownloader.download_image_from_file(existing_file, fake_dir)
+    assert fake_dir.exists()
+    assert len([*fake_dir.iterdir()]) == 5
+
+
+def test_download_from_file_partly_successful(fake_dir, mixed_urls):
+    with patch("logging.Logger.error") as mock_logger:
+        ImageDownloader.download_image_from_file(mixed_urls, fake_dir)
+
+        mock_logger.assert_called()
+        logger_calls = mock_logger.call_args_list
+
+        first_call = logger_calls[0]
+        second_call = logger_calls[1]
+
+        first_call_arguments = first_call[0]
+        second_call_arguments = second_call[0]
+
+        assert "ConnectionError" in first_call_arguments[0]
+        assert "HTTPError" in second_call_arguments[0]
+        assert fake_dir.exists()
+        assert len([*fake_dir.iterdir()]) == 1
+
+
+def test_download_from_file_not_existing_file(fake_dir, not_existing_file):
+    with patch("logging.Logger.error") as mock_logger:
+        with pytest.raises(FileNotFoundError):
+            ImageDownloader.download_image_from_file(not_existing_file, fake_dir)
+
+    mock_logger.assert_called_once()
+    mock_logger.assert_called_with("File does not exist")
+    assert fake_dir.exists()
+
+
+def test_download_from_file_not_existing_save_path(get_not_existing_dir, existing_file):
+    with patch("logging.Logger.error") as mock_logger:
+        with pytest.raises(FileNotFoundError):
+            ImageDownloader.download_image_from_file(
+                existing_file, get_not_existing_dir
+            )
+
+    mock_logger.assert_called_once()
+    mock_logger.assert_called_with("Path does not exist")
